d6c8be44fd04c09a9c4e35fdf6aa9a2043d9f872
Endeyr <aaron.thompson42594@gmail.com>
2022-08-01 12:11:58 -0400

    added devise and created view pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def contacts
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   get 'static_pages/home'
+  get    "/help",   to: "static_pages#help"
+  get    "/about",  to: "static_pages#about"
+  get    "/contact",to: "static_pages#contact"
   root "static_pages#home"
   get '*path' => redirect('/')
 end
